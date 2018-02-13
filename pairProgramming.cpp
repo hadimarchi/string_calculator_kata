e6cc080eeea471395c896e19524ed8e4c682d761
hadimarchi <hal.dimarchi55@gmail.com>
2018-02-12 16:27:20 -0900

    trying to pass fourth test

--- pairProgramming.cpp
+++ pairProgramming.cpp
@@ -1,19 +1,31 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <vector>
+using std::vector;
 #include <string>
 using std::string;
 
+string delimiter = ",";
 int add(const string &addable){
-    string firstNum;
+    string modified_add = addable;
+    modified_add += ",";
+    vector<int> nums{};
     if(addable.size()==0){
         return 0;
     }
-    for(auto i : addable){
-        string hold;
 
-
+    auto num1 = stoi(modified_add.substr(0, modified_add.find(delimiter)));
+    int num2;
+    try{
+      std::cout<<modified_add;
+      num2 = stoi(modified_add.substr(1, modified_add.find(delimiter)));
+      std::cout<<num2;
     }
-    return 1;
+    catch(...){
+      return num1;
+    }
+
+    return num1 + num2;
 }
 
 TEST_CASE( "add null str [add]" ) {
